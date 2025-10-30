8b07c46b9a863b56387ae8095f585e03a759af39
Mahmoud9-dev <mahmoud0114829@gmail.com>
2025-10-31 01:23:58 +0200

    Implement home page with Trading View widgets

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,5 +1,7 @@
 import TradingViewWidget from "@/components/TradingViewWidget";
 import {
+  HEATMAP_WIDGET_CONFIG,
+  MARKET_DATA_WIDGET_CONFIG,
   MARKET_OVERVIEW_WIDGET_CONFIG,
   TOP_STORIES_WIDGET_CONFIG,
 } from "@/lib/constants";
@@ -20,9 +22,8 @@ const Home = () => {
         </div>
         <div className="md:col-span xl:col-span-2">
           <TradingViewWidget
-            title="Stock Heatmap"
             scriptUrl={`${scriptUrl}stock-heatmap.js`}
-            config={MARKET_OVERVIEW_WIDGET_CONFIG}
+            config={HEATMAP_WIDGET_CONFIG}
             className="custom-chart"
             height={600}
           />
@@ -32,7 +33,6 @@ const Home = () => {
       <section className="grid w-full gap-8 home-section">
         <div className=" h-full md:col-span-1 xl:col-span-1">
           <TradingViewWidget
-            title="Market Overview"
             scriptUrl={`${scriptUrl}timeline.js`}
             config={TOP_STORIES_WIDGET_CONFIG}
             height={600}
@@ -42,7 +42,7 @@ const Home = () => {
           <TradingViewWidget
             title="Stock Heatmap"
             scriptUrl={`${scriptUrl}market-quotes.js`}
-            config={MARKET_OVERVIEW_WIDGET_CONFIG}
+            config={MARKET_DATA_WIDGET_CONFIG}
             height={600}
           />
         </div>
